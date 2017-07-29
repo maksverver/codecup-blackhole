ae65057dd2f588c82c468e3da5a27049e7130435
Maks Verver <maksverver@geocities.com>
2017-07-29 23:22:46 +0200

    Implement moving & long polling for state changes.

--- play.js
+++ play.js
@@ -1,9 +1,43 @@
 let globalStateId = stripHash(window.location.hash || "");
 let globalBaseUrl = window.location.href.substr(0, window.location.href.lastIndexOf('/'));
 let globalStateUrl = globalBaseUrl + '/states/' + globalStateId;
+let globalLastETag = null;
 
 function stripHash(s) {
   return s.startsWith('#') ? s.substr(1) : s;
+}
+
+function pollForChange(etag) {
+  if (!etag) {
+    alert('Missing ETag response header! Long polling disabled.');
+    return;
+  }
+  globalLastETag = etag;
+  let request = new XMLHttpRequest();
+  request.addEventListener('error', function() {
+    alert('XMLHttpRequest failed!');
+  });
+  request.addEventListener('load', function() {
+    if (request.status == 304) {  // Not Modified
+      pollForChange(etag);
+    } else if (request.status != 200) {
+      alert('Game retrieval request failed!\n' +
+        'Server returned status code: ' + request.status + '.\n' +
+        'Reload the page to continue.');
+    } else {
+      let stateString = request.responseText;
+      let newState = decodeState(stateString);
+      if (!newState) {
+        alert('Invalid state string: ' + JSON.stringify(stateString));
+      } else {
+        updateState(newState);
+      }
+      pollForChange(request.getResponseHeader('ETag'));
+    }
+  });
+  request.open('GET', globalStateUrl);
+  request.setRequestHeader('If-None-Match', etag);
+  request.send();
 }
 
 function setInitialStateString(stateString) {
@@ -15,14 +49,6 @@ function setInitialStateString(stateString) {
 
   updateState(initialState);
 
-  // TODO
-  // TODO
-  // TODO
-  // Start long polling for state changes.
-  // TODO
-  // TODO
-  // TODO
-
   boardCanvas.addEventListener('fieldClick', function(event) {
     let u = event.detail.u;
     let v = event.detail.v;
@@ -31,14 +57,26 @@ function setInitialStateString(stateString) {
       console.log('Invalid move: ' + coordsToString(u, v) + '=' + globalSelectedPiece);
       return;
     }
-    // TODO
-    // TODO
-    // TODO
-    alert('TODO -- PUT request');
-    // PUT updated state to server.
-    // TODO
-    // TODO
-    // TODO
+
+    let request = new XMLHttpRequest();
+    request.addEventListener('error', function() {
+      alert('XMLHttpRequest failed!');
+    });
+    request.addEventListener('load', function() {
+      if (request.status == 412) {
+        alert('Game state has changed! Wait for the new state, and try again?');
+      } else if (request.status != 200) {
+        alert('Game retrieval request failed!\n' +
+          'Server returned status code: ' + request.status + '.\n' +
+          'Reload the page to continue.');
+      } else {
+        // Nothing to be done. We should be polling for state changes, and
+        // will receive the update that way.
+      }
+    });
+    request.open('PUT', globalStateUrl);
+    request.setRequestHeader('If-Match', globalLastETag);
+    request.send(encodeState(newState));
   });
 
   piecesCanvas.addEventListener('pieceClick', function(event) {
@@ -65,10 +103,10 @@ function initialize() {
         'Reload the page to continue.');
     } else {
       setInitialStateString(request.responseText);
+      pollForChange(request.getResponseHeader('ETag'));
     }
   });
   request.open('GET', globalStateUrl);
-  request.setRequestHeader('Content-Type', 'text/plain');
   request.send();
 }
 
